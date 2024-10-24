fc322917a9c0ef000cec68dbc8018ddd72bdd342
Matheus Couto <matheus.s.couto6@aluno.senai.br>
2024-10-24 11:01:02 -0300

    feat: criação do botão de modo noturno

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-const icone = document.getElementById("icone");
-let darkMode = false;
-
-icone.addEventListener("click", function() {
-    if (darkMode) {
-        document.body.style.background = "white";
-        document.body.style.color = "black";
-        darkMode = false;
-    } else {
-        document.body.style.background = "black";
-        document.body.style.color = "white";
-        darkMode = true;
+document.addEventListener('DOMContentLoaded', (event) => {
+    const toggleSwitch = document.getElementById('toggle');
+    let darkMode = false;
+  
+    // Função para alternar entre modo claro e escuro
+    function toggleDarkMode() {
+        if (darkMode) {
+            document.body.style.background = "white";
+            document.body.style.color = "black";
+            darkMode = false;
+        } else {
+            document.body.style.background = "#2e3e41ff";
+            document.body.style.color = "white";
+            darkMode = true;
+        }
     }
-});
+  
+    // Evento de mudança no botão on/off
+    toggleSwitch.addEventListener('change', function() {
+        toggleDarkMode();
+    });
+  });
