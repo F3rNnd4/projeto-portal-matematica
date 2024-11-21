689f9c247d4b83d5ffcb75ec1a2888157f93fa51
vinicius valverde <vinicius.valverde@aluno.senai.br>
2024-11-21 17:23:35 -0300

    Linha da home e update script

--- script.js
+++ script.js
@@ -5,17 +5,21 @@ let modoNoturno = localStorage.getItem("modoNoturno") === "true";
 function aplicarmodoNoturno() {
     document.body.style.background = "#2F2F2F";
     document.body.style.color = "white";
+    icone.src = "./imgHeader/diurno.png"
 }
 
 // Função para removerr o modo noturno
 function removermodoNoturno() {
     document.body.style.background = "white";
     document.body.style.color = "black";
+    icone.src = "./imgHeader/noturno.png"
 }
 
 // Verifica o estado do modo noturno ao carregar/recarregar a página
 if (modoNoturno) {
     aplicarmodoNoturno();
+} else {
+    removermodoNoturno();
 }
 
 icone.addEventListener("click", function () {
